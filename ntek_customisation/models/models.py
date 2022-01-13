8090d7ed81f64eb372e6d3824f3b712ef37ea507
Sibghatullah <smsibghatullah@gmail.com>
2022-01-13 11:49:17 +0500

    added fields

--- ntek_customisation/models/models.py
+++ ntek_customisation/models/models.py
@@ -7,11 +7,13 @@ class ProjectTask(models.Model):
     contract_id = fields.Many2one('contract', string='Contract')
     ticket_no = fields.Char(string = 'Ticket No.')
     scope_of_work = fields.Char(string = 'Scope Of Work')
-    travel_start = fields.Datetime(string = 'Travel Start')
-    arrival_on_site = fields.Datetime(string = 'Arrival On Site')
-    repair_start = fields.Datetime(string = 'Repair Start')
-    repair_stop = fields.Datetime(string = 'Repair Stop')
-    leave_site = fields.Datetime(string = 'Leave Site')
+    travel_start = fields.Datetime(string = 'Travel Start') #Leaving For SITE
+    arrival_on_site = fields.Datetime(string = 'Arrival On Site') #Arrival On SITE
+    repair_start = fields.Datetime(string = 'Repair Start') #Work Start
+    repair_stop = fields.Datetime(string = 'Repair Stop') #Work Stop
+    sign_off_taken = fields.Datetime(string='Sign Off Taken') #Sign Off Taken
+    sign_off_submitted = fields.Datetime(string='Sign Off Submitted') #Sign Off Submitted
+    leave_site = fields.Datetime(string = 'Leave Site') #Levae Site
     travel_stop = fields.Datetime(string = 'Travel Stop')
     km_start = fields.Float(string = 'KM Start')
     km_till_date = fields.Float(string = 'KM Till Date')
